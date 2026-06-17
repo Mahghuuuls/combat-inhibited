50a9d66a1bcd2e5d271c20925c38393cbff59a2f
Arthur Carlos <arthcrs@gmail.com>
2026-06-17 20:09:25 -0300

    Migrate to a modern Forge Gradle template

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## [1.0.2] - 2026-06-17
+
+### Changed
+- Migrated the project to the Mahghuuuls Forge 1.12.2 template structure.
--- src/main/java/com/mahghuuuls/combatinhibited/CombatInhibited.java
+++ src/main/java/com/mahghuuuls/combatinhibited/CombatInhibited.java
@@ -29,11 +29,11 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-@Mod(modid = CombatInhibited.MOD_ID, name = CombatInhibited.NAME, version = CombatInhibited.VERSION, dependencies = CombatInhibited.DEPENDENCIES)
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION, dependencies = CombatInhibited.DEPENDENCIES)
 public class CombatInhibited {
-    public static final String MOD_ID = "combatinhibited";
-    public static final String NAME = "Combat Inhibited";
-    public static final String VERSION = "1.0.2";
+    public static final String MOD_ID = Tags.MOD_ID;
+    public static final String NAME = Tags.MOD_NAME;
+    public static final String VERSION = Tags.VERSION;
     public static final String DEPENDENCIES = "required-after:inhibited";
 
     @EventHandler
